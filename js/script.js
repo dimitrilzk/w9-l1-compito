3566db270424e5c5278f172d349c44a32f6be092
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-24 16:15:55 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 "use strict";
 let giocatore1 = 7;
 let giocatore2 = 35;
+let numeroCasuale;
+numeroCasuale = Math.round((Math.random() * (100 - 1) + 1));
+console.log(numeroCasuale);
--- ts/script.ts
+++ ts/script.ts
@@ -1,2 +1,5 @@
-let giocatore1 = 7; 
-let giocatore2 = 35;
+let giocatore1:number = 7; 
+let giocatore2:number = 35;
+let numeroCasuale:number;
+numeroCasuale = Math.round((Math.random() * (100-1)+1));
+console.log(numeroCasuale);
