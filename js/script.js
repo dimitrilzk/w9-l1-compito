641ef37977ccf5ec4c361f3267001e5e9ccb7918
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-24 16:24:14 +0200

    2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ let giocatore2 = 35;
 let numeroCasuale;
 numeroCasuale = Math.round((Math.random() * (100 - 1) + 1));
 console.log(numeroCasuale);
+function vincitore() {
+    if (giocatore1 === numeroCasuale || giocatore2 === numeroCasuale) {
+        console.log("Complimenti hai indovinato!");
+    }
+    else if (giocatore1 !== numeroCasuale || giocatore2 !== numeroCasuale) {
+        console.log("Mi dispiace non hai indovinato!");
+    }
+    return;
+}
+console.log(vincitore());
--- ts/script.ts
+++ ts/script.ts
@@ -3,3 +3,12 @@ let giocatore2:number = 35;
 let numeroCasuale:number;
 numeroCasuale = Math.round((Math.random() * (100-1)+1));
 console.log(numeroCasuale);
+function vincitore() {
+    if(giocatore1 === numeroCasuale || giocatore2 === numeroCasuale) {
+        console.log("Complimenti hai indovinato!")
+    } else if(giocatore1 !== numeroCasuale || giocatore2 !== numeroCasuale) {
+        console.log("Mi dispiace non hai indovinato!")
+    }
+    return
+}
+console.log(vincitore())
